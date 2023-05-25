0cbc582d4dcfd931b41e0edfccee0760b6cefa84
Tushar Khanduri <62638544+Tushar-K24@users.noreply.github.com>
2023-05-25 21:58:12 +0530

    fix: http request function

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
+import functions_framework
 from similarProducts import findSimilarItems
 
 
+@functions_framework.http
 def clothesSuggestions(request):
-    return "Hello World"
-    request_json = request.get_json()
+    # request_json = request.get_json()
     description, limit = None, 10
 
     # load description
     if request.args and "description" in request.args:
         description = request.args.get("description")
-    elif request_json and "description" in request_json:
-        description = request_json["description"]
+    # elif request_json and "description" in request_json:
+    # description = request_json["description"]
 
     # load limit(if exists)
     if request.args and "limit" in request.args:
         limit = request.args.get("limit")
-    elif request_json and "limit" in request_json:
-        limit = request_json["limit"]
+    # elif request_json and "limit" in request_json:
+    # limit = request_json["limit"]
 
     if not description:
         return "description not found", 404
